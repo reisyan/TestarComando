db571545b62824ea00da1f9d994ed87685e6fd7f
Yan Reis <yanreisdev@gmail.com>
2023-03-30 22:49:46 -0300

    Chamar comandos boolean.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,14 @@ public class Main {
 
         boolean comunicando;
         System.out.println("Responda somente com true ou false:");
-
+        System.out.println("------------------------------------------------------");
         System.out.println("HammerHead Está comunicando:");
         comunicando = entrada.nextBoolean();
 
-
         if (comunicando) {
-            System.out.println("Habilitar botão gravar");
-            System.out.println("Habilitar botão desliga");
-            System.out.println("Ocultar mensagens de erro");
+            System.out.println("Habilitar botão gravar.");
+            System.out.println("Habilitar botão desliga.");
+            System.out.println("Ocultar mensagens de erro.");
         }
 
         boolean falhadeComunicacao;
@@ -27,17 +26,17 @@ public class Main {
         falhadeComunicacao = entrada.nextBoolean();
 
         if (falhadeComunicacao) {
-            System.out.println("Desabilita botão Grava");
-            System.out.println("Desabilita Botão Desliga");
-            System.out.println("Exibe mensagem de Falha de Comunicação");
+            System.out.println("Desabilita botão Grava.");
+            System.out.println("Desabilita Botão Desliga.");
+            System.out.println("Exibe mensagem de Falha de Comunicação.");
         }
         boolean gravandoemDisco;
         System.out.printf("HammerHead Está Comunicando && Gravando disco:");
         gravandoemDisco = entrada.nextBoolean();
 
         if (comunicando && gravandoemDisco) {
-            System.out.println("Desabilita Botão Desliga");
-            System.out.println("Habilita Botão Stop");
+            System.out.println("Desabilita Botão Desliga.");
+            System.out.println("Habilita Botão Stop.");
         }
 
         boolean botaoGravarApertado;
@@ -45,9 +44,9 @@ public class Main {
         botaoGravarApertado = entrada.nextBoolean();
 
         if (botaoGravarApertado) {
-            System.out.println("Inicia gravação");
-            System.out.printf("Desabilita Botão Desliga");
-            System.out.printf("Desabilita Botão Gravar");
+            System.out.println("Inicia gravação.");
+            System.out.println("Desabilita Botão Desliga.");
+            System.out.println("Desabilita Botão Gravar.");
 
         }
         boolean botaoStopApertado;
@@ -55,18 +54,18 @@ public class Main {
         botaoStopApertado = entrada.nextBoolean();
 
         if (botaoStopApertado) {
-            System.out.println("Interrompe gravação");
-            System.out.printf("Habilita Botão Desliga");
-            System.out.printf("Desabilita Botão Gravar");
+            System.out.println("Interrompe gravação.");
+            System.out.println("Habilita Botão Desliga.");
+            System.out.println("Desabilita Botão Gravar.");
         }
         boolean botaoDesligaApertado;
-        System.out.printf("HammerHead Está com Botão Desliga está Apertado:");
+        System.out.println("HammerHead Está com Botão Desliga está Apertado:");
         botaoDesligaApertado = entrada.nextBoolean();
 
         if (botaoDesligaApertado) {
-            System.out.println("Desabilita botão Grava");
-            System.out.printf("Desabilita Botão Desliga");
-            System.out.printf("Exibe mensagem de Desligando");
+            System.out.println("Desabilita botão Grava.");
+            System.out.println("Desabilita Botão Desliga.");
+            System.out.println("Exibe mensagem de Desligando.");
         }
     }
 }
